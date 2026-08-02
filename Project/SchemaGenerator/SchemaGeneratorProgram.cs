af182964802e798c16c818096404e4395597706c
wknam <nwk1541@gmail.com>
2026-08-02 19:45:48 +0900

    feat : 엑셀 조회를 위한 구조 및 파일 추가, 경로 조회 테스트 코드 작성

--- Project/SchemaGenerator/SchemaGeneratorProgram.cs
+++ Project/SchemaGenerator/SchemaGeneratorProgram.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("SchemaGenerator: Hello World!");
+            string rootDirectoryPath = Core.PathUtil.GetRootDirectoryPath();
+            string excelDirectoryPath = Core.PathUtil.GetExcelDirectoryPath();
+            string protoDirectoryPath = Core.PathUtil.GetProtoDirectoryPath();
+            string outputDirectoryPath = Core.PathUtil.GetOutputDirectoryPath();
+
+            Console.WriteLine("경로 조회 성공");
+            Console.WriteLine($"작업 루트: {rootDirectoryPath}");
+            Console.WriteLine($"Excel 경로: {excelDirectoryPath}");
+            Console.WriteLine($"Proto 경로: {protoDirectoryPath}");
+            Console.WriteLine($"Output 경로: {outputDirectoryPath}");
         }
     }
 }
